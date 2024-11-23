7d97c288b318f349f13293cb71de1de6c48cbd1e
damenor <damenor.develop@gmail.com>
2024-11-23 18:35:29 +0100

    Add hero animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,25 +1,60 @@
-import { Button, Pointer } from '@/components'
+'use client'
+import Image from 'next/image'
+import { useEffect } from 'react'
+import { motion, useAnimate } from 'framer-motion'
+
 import designExample1Image from '@/assets/images/design-example-1.png'
 import designExample2Image from '@/assets/images/design-example-2.png'
-import Image from 'next/image'
+import cursorYoutubeImage from '@/assets/images/cursor-you.svg'
+import { Button, Pointer } from '@/components'
 
 export const Hero = () => {
-  return (
-    <section className="py-24 overflow-x-clip pt-40 md:pt-24">
-      <div className="container relative">
-        <div className="hidden lg:block absolute -left-32 top-16">
-          <Image src={designExample1Image} alt="Design example 1 image" />
-        </div>
-        <div className="hidden lg:block absolute -right-64 -top-16">
-          <Image src={designExample2Image} alt="Design example 2 image" />
-        </div>
+  const [leftDesignScope, leftDesignAnimate] = useAnimate()
+  const [leftPointerScope, leftPointerAnimate] = useAnimate()
+  const [rightDesignScope, rightDesignAnimate] = useAnimate()
+  const [rightPointerScope, rightPointerAnimate] = useAnimate()
 
-        <div className="hidden lg:block absolute left-56 top-96">
+  useEffect(() => {
+    leftDesignAnimate([
+      [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ])
+    leftPointerAnimate([
+      [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+      [leftPointerScope.current, { x: 0, y: [0, 16, 0] }, { duration: 0.5, ease: 'easeInOut' }],
+    ])
+    rightDesignAnimate([
+      [rightDesignScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+      [rightDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ])
+    rightPointerAnimate([
+      [rightPointerScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+      [rightPointerScope.current, { y: 0, x: 175 }, { duration: 0.5 }],
+      [rightPointerScope.current, { x: 0, y: [0, 20, 0] }, { duration: 0.5, ease: 'easeInOut' }],
+    ])
+  }, [])
+
+  return (
+    <section
+      className="py-24 overflow-x-clip pt-40 md:pt-24"
+      style={{
+        cursor: `url(${cursorYoutubeImage.src}), auto`,
+      }}
+    >
+      <div className="container relative">
+        <motion.div ref={leftDesignScope} className="hidden lg:block absolute -left-32 top-16" initial={{ opacity: 0, y: 100, x: -100 }} drag>
+          <Image src={designExample1Image} alt="Design example 1 image" draggable={false} />
+        </motion.div>
+        <motion.div ref={leftPointerScope} className="hidden lg:block absolute left-56 top-96" initial={{ opacity: 0, y: 100, x: -200 }}>
           <Pointer name="Andrea" />
-        </div>
-        <div className="hidden lg:block absolute top-0 right-80">
+        </motion.div>
+        <motion.div ref={rightDesignScope} className="hidden lg:block absolute -right-64 -top-16" initial={{ opacity: 0, y: 100, x: 100 }} drag>
+          <Image src={designExample2Image} alt="Design example 2 image" draggable={false} />
+        </motion.div>
+        <motion.div ref={rightPointerScope} className="hidden lg:block absolute top-0 right-80" initial={{ opacity: 0, y: 100, x: 275 }}>
           <Pointer name="Bryan" color="red" />
-        </div>
+        </motion.div>
 
         <div className="flex justify-center">
           <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
